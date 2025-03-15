37914b17cba04c4c09518c36b1ed20e1882953dd
Smeet-Chothani <smeetchothani16@gmail.com>
2025-03-15 18:08:54 +0530

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -3,5 +3,8 @@
 # Install system dependencies for PyAudio
 apt-get update && apt-get install -y portaudio19-dev
 
+# Ensure we have the latest pip
+pip install --upgrade pip
+
 # Install Python dependencies
 pip install -r requirements.txt
